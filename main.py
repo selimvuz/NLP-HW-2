335b7b8ef3960c7662e8d67223acc5b5e348a8f3
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2024-03-29 12:12:36 +0300

    Path config

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pandas as pd
-import seaborn as sns
-from palmerpenguins import load_penguins
-sns.set_style('whitegrid')
-penguins = load_penguins()
-print(penguins.head())
+
+# Excel dosyasını oku
+file_path = 'dataset/soru_cevap.xlsx'
+df = pd.read_excel(file_path)
+
+# Tercih sütununa göre filtrele ve say
+insan_cevabi_sayisi = (df['tercih'] == 1).sum()
+makine_cevabi_sayisi = (df['tercih'] == 2).sum()
+ikisi_de_iyi_sayisi = (df['tercih'] == 3).sum()
+
+# Sonuçları yazdır
+print("İnsan cevabının tercih edildiği sayı:", insan_cevabi_sayisi)
+print("Makine cevabının tercih edildiği sayı:", makine_cevabi_sayisi)
+print("Her iki cevabın da iyi olduğu durumların sayısı:", ikisi_de_iyi_sayisi)
